a71749205749cf85aa9558a5e51cb46595e21eef
GuilherMenezesdaSilva <rm89384@fiap.com.br>
2022-09-09 11:57:05 -0300

    Conexão com BD + Adição de valores + consulta da tabela

--- loja/src/main/java/br/com/fiap/PrimaryController.java
+++ loja/src/main/java/br/com/fiap/PrimaryController.java
@@ -1,11 +1,12 @@
 package br.com.fiap;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
-
 
 import br.com.fiap.model.Usuario;
 import javafx.fxml.FXML;
@@ -13,7 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 
 public class PrimaryController implements Initializable{
 
@@ -40,6 +40,16 @@ public class PrimaryController implements Initializable{
         try{
             System.out.println("Conectando..");
             Connection conexao = DriverManager.getConnection(url, username, senha);
+            Statement comando = conexao.createStatement();
+
+            String sql = String.format(   
+                "INSERT INTO T_DDD_LOJA_USUARIO (id, nome, email, senha, perfil) VALUES (SEQ_USUARIO.nextval, '%s', '%s', '%s', '%s')",
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.getSenha(),
+                usuario.getPerfil()
+            );
+            comando.execute(sql);
             conexao.close();
             System.out.println("Conectado");
         }catch(SQLException e){
@@ -51,5 +61,9 @@ public class PrimaryController implements Initializable{
     public void initialize(URL arg0, ResourceBundle arg1) {
         choiceBoxPerfil.getItems().addAll("Vendedor");
         choiceBoxPerfil.getItems().addAll("Gerente");        
+    }
+
+    public void abrirListadeUsuarios() throws IOException{
+        App.setRoot("secondary");
     }
 }
